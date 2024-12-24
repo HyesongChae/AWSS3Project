b70370f8ccc034c79dd7a5b8562015ed459b8221
hye_song <37098936+HyesongChae@users.noreply.github.com>
2024-12-24 16:17:48 +0900

    Update S3Service.java
    window -> ubuntu 경로 수정

--- src/main/java/file/service/S3Service.java
+++ src/main/java/file/service/S3Service.java
@@ -46,7 +46,7 @@ public class S3Service {
 		}
 		
 		// DB 저장
-		String filePath = "C://CE//97.data//" + DIR_NAME;
+		String filePath = "/home/ubuntu/" + DIR_NAME;
 		String attachmentOriginalFileName = file.getOriginalFilename();
 		UUID uuid = UUID.randomUUID();
 		String attachmentFileName = uuid.toString() + "_" + attachmentOriginalFileName;
